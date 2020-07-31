16296b7f0f03c1bf222718e5e3b4f55f19026bbf
DhruvGheewala <dhruvgheewala.4g@gmail.com>
2020-08-01 01:23:11 +0530

    updated !!

---
+++ scripts/compile.js
@@ -0,0 +1,43 @@
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
+
+function extract_lang(url) {
+    url = reverse(url);
+    var n = url.search('#');
+    if (n == -1) {
+        // Language isn't selected
+        return false;
+    }
+    str = '';
+    for (let i = 0; i < n; i++)
+        str += url[i];
+    return reverse(str);
+}
+
+function getURL() {
+    var cur_url = window.location.href;
+    return cur_url;
+}
+
+function compilerun() {
+
+    var lang = extract_lang(getURL());
+    console.log('lang : ' + lang);
+    if (lang) {
+
+        var tag = document.getElementById('compile-run');
+        if (tag == 'Run') {
+            // Run the compiled code
+
+            // Redirect
+        } else {
+            // Start Compilation
+
+            // At the end change compile buttont to submit button
+            tag.textContent = 'Run';
+        }
+        return;
+    }
+    alert('Please Select The Language !!');
+}
